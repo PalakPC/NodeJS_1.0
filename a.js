f670062078ba80575b8e2a8aae86e22c801f79f2
palak <no.1palak@gmail.com>
2014-01-21 17:19:27 +0530

    TCP socket done

--- a.js
+++ a.js
@@ -1,45 +1,54 @@
 var http = require("http"),
-	url = require("url"),
-	path = require("path"),
-        mysql = require("mysql"),
-	fs = require("fs")
-	port = process.argv[2] || 8888;
+    url = require("url"),
+    express = require("express");
+    path = require("path"),
+    mysql = require("mysql"),
+    fs = require("fs")
+    port = process.argv[2] || 8888;
 
 var connection = mysql.createConnection({
-	host : 'localhost',
-	database : 'myfiles',
-	user : 'root',
-	password: 'fire'
+    host : 'localhost',
+    database : 'myfiles',
+    user : 'root',
+    password: 'fire'
 });
+
+//var app = express();
 
 http.createServer(function(request, response) {				     
 	var uri = url.parse(request.url).pathname, 
-		filename = path.join(process.cwd(), uri);
-
+      	    filename = path.join(process.cwd(), uri);
 	path.exists(filename, function(exists) {
 		if(!exists) {
-			response.writeHead(404, {"Content-Type": "text/plain"});
+			response.writeHead("Content-Type", "text/plain");
 			response.write("404 Not Found\n");
 			response.end();
 			return;
 		}
-		
 		if (fs.statSync(filename).isDirectory()) 
 			filename += '/index.html';
-
-		fs.readFile(filename, "binary", function(err, file) {
-			if(err) {        
-				response.writeHead(500, {"Content-Type": "text/plain"});
-				response.write(err + "\n");
-				response.end();
-				return;
+		connection.query("select * from my_filenames", function(err, rows){
+			if(err!=null){
+				response.end("Query error: "+err);
 			}
-			response.writeHead(200);
-			response.write(file, "binary");
-			response.end();
+			else{
+				var object = rows[1];
+				console.log(object);
+				//response.writeHead(200, {'content-type': 'application/json'});
+				fs.readFile(filename, "binary", function(error, file) {
+					if(error) {        
+						response.writeHead(500, {"Content-Type": "text/html"});
+						response.write(err + "\n");
+						response.end();
+						return;
+					}	
+					response.writeHead(200);
+					//response.send(JSON.stringify(obj));
+					response.write(file, "binary");
+					response.end();
+				});
+			}
 		});
 	});
 }).listen(parseInt(port, 10));
- 
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
-
